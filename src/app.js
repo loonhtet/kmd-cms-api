a2417736d242991013ba5e80d0ef17d67517826f
Loon Htet <loonhtet.dev@gmail.com>
2026-02-28 18:56:32 +0700

    update: cors policy

--- src/app.js
+++ src/app.js
@@ -32,8 +32,12 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
