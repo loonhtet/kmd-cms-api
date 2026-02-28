8cb4456f3cd45bebcd24b6ceb8bf014858cda342
Loon Htet <loonhtet.dev@gmail.com>
2026-03-01 02:41:17 +0700

    add: cors frontend domain

--- src/app.js
+++ src/app.js
@@ -30,7 +30,11 @@ const globalLimiter = rateLimit({
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://e-tutoring-seven.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
